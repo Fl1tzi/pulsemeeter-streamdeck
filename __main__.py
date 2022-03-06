03504122e6f96f76a407282716392b2f9d219a27
Fl1tzi <kontakt@tgerber.net>
2022-03-06 17:51:57 +0100

    multiprocessing, connections and more settings.

--- __main__.py
+++ __main__.py
@@ -8,14 +8,13 @@ from subprocess import Popen, PIPE, run
 import math
 from time import sleep
 import re
+import multiprocessing.dummy as mp
 
 from PIL import Image, ImageDraw, ImageFont
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.ImageHelpers import PILHelper
 
 import settings
-
-
 
 device_1 = settings.device_1
 device_2 = settings.device_2
@@ -24,12 +23,14 @@ device_4 = settings.device_4
 device_5 = settings.device_5
 
 volume_step = settings.volume_step
+label_seperator = settings.label_seperator
+margins = settings.margins
 
 # Generates a custom tile with run-time generated text and custom image via the
 # PIL module.
 def render_key_image(deck, icon_filename, font_filename, label_text):
     icon = Image.open(icon_filename)
-    image = PILHelper.create_scaled_image(deck, icon, margins=[25, 0, 10, 0])
+    image = PILHelper.create_scaled_image(deck, icon, margins=margins)
 
     # Load a custom TrueType font and use it to overlay the key index, draw key
     # label onto the image a few pixels from the bottom of the key.
@@ -69,6 +70,10 @@ def get_primary(device):
     process = Popen(['pulsemeeter', 'get', 'primary', device], stdout=PIPE)
     return process.communicate()[0].decode().strip()
 
+def get_connect(device, device2):
+    process = Popen(['pulsemeeter', 'get', 'connect', device, device2], stdout=PIPE)
+    return process.communicate()[0].decode().strip()
+
 # get the picture for the speaker
 def get_speaker_pic(device, device_type):
     state = get_mute(device)
@@ -88,6 +93,12 @@ def get_button_pic(value):
         return "toggle-fill.png"
     else:
         return "toggle-line.png"
+
+def get_checkbox_pic(value):
+    if value == 'True':
+        return "checkbox-blank-fill.png"
+    else:
+        return "checkbox-blank-line.png"
 
 
 # get a device by the label (eg. one/two/three/...)
@@ -145,9 +156,9 @@ def get_current_key_style(deck, key, load_data=False):
                     #device_type = device[1]
                     #icon = get_speaker_pic(device[0], device_type)
                     if load_data:
-                        label = f"{get_volume(device[0])}-{device_label}"
+                        label = f"{get_volume(device[0])}{label_seperator}{device_label}"
                     else:
-                        label = f"0-{device_label}"
+                        label = f"0{label_seperator}{device_label}"
             except:
                 name = ""
                 icon = "Empty.png"
@@ -161,10 +172,20 @@ def get_current_key_style(deck, key, load_data=False):
             name = device_1[3]
             icon = "close-line.png"
             label = "close"
+        # RNNOISE
         elif key == one_down:
-            name = device_1[3]+"-"
-            icon = "Empty.png"
-            label = ""
+            if re.match('^(hi)+', dev[0]):
+                name = device_1[3]+"-"
+                if load_data:
+                    icon = get_button_pic(get_rnnoise(dev[0]))
+                else:
+                    icon = "refresh-line.png"
+                label = "rnnoise"
+            else:
+                name = ""
+                icon = "Empty.png"
+                label = ""
+        # MUTE
         elif key == one_up:
             name = device_1[3]+"+"
             if load_data:
@@ -178,77 +199,132 @@ def get_current_key_style(deck, key, load_data=False):
             label = ""
         elif key == two_down:
             name = device_2[3]+"-"
-            if re.match('^(b|vi)+', dev[0]):
-                if load_data:
-                    icon = get_button_pic(get_primary(dev[0]))
-                else:
-                    icon = "refresh-line.png"
-                label = "primary"
             icon = "Empty.png"
             label = ""
+        # EQ
         elif key == two_up:
-            name = device_2[3]+"+"
             if re.match('^(a|b)+', dev[0]):
+                name = device_2[3]+"+"
                 if load_data:
                     icon = get_button_pic(get_eq(dev[0]))
                 else:
                     icon = "refresh-line.png"
                 label = "EQ"
             else:
+                name = ""
                 icon = "Empty.png"
                 label = ""
-        if key == three_base:
+        elif key == three_base:
             name = device_3[3]
             icon = "Empty.png"
             label = ""
+            if settings.show_device:
+                label = f"Device:\n{dev[0].upper()}"
+            else:
+                label = ""
         elif key == three_down:
             name = device_3[3]+"-"
             icon = "Empty.png"
             label = ""
+        # PRIMARY
         elif key == three_up:
-            name = device_3[3]+"+"
-            if re.match('^(hi)+', dev[0]):
+            if re.match('^(b|vi)+', dev[0]):
+                name = device_3[3]+"+"
                 if load_data:
-                    icon = get_button_pic(get_rnnoise(dev[0]))
+                    icon = get_button_pic(get_primary(dev[0]))
                 else:
                     icon = "refresh-line.png"
-                label = "rnnoise"
+                label = "primary"
             else:
                 icon = "Empty.png"
                 label = ""
-        elif key == four_base:
-            name = device_4[3]
-            icon = "Empty.png"
-            label = ""
-        elif key == four_down:
-            name = device_4[3]+"-"
-            icon = "Empty.png"
-            label = ""
-        elif key == four_up:
-            name = device_4[3]+"+"
-            icon = "Empty.png"
-            label = ""
-        elif key == five_base:
-            name = device_5[3]
-            icon = "Empty.png"
-            label = ""
-        elif key == five_down:
-            name = device_5[3]+"-"
-            icon = "Empty.png"
-            label = ""
-        elif key == five_up:
-            name = device_5[3]+"+"
-            icon = "Empty.png"
-            label = ""
+                name = ""
+        else:
+            # CONNECTIONS
+            if re.match('^(hi|vi)+', dev[0]):
+                connection_allowed = True
+            else:
+                connection_allowed = False
 
-
+            if key == four_base:
+                if connection_allowed:
+                    name = device_4[3]
+                    if load_data:
+                        icon = get_checkbox_pic(get_connect(dev[0], 'a1'))
+                    else:
+                        icon = "checkbox-blank-line.png"
+                    label = "A1"
+                else:
+                    name = ""
+                    icon = "Empty.png"
+                    label = ""
+            elif key == four_down:
+                if connection_allowed:
+                    name = device_4[3]+"-"
+                    if load_data:
+                        icon = get_checkbox_pic(get_connect(dev[0], 'a2'))
+                    else:
+                        icon = "checkbox-blank-line.png"
+                    label = "A2"
+                else:
+                    name = ""
+                    icon = "Empty.png"
+                    label = ""
+            elif key == four_up:
+                if connection_allowed:
+                    name = device_4[3]+"+"
+                    if load_data:
+                        icon = get_checkbox_pic(get_connect(dev[0], 'a3'))
+                    else:
+                        icon = "checkbox-blank-line.png"
+                    label = "A3"
+                else:
+                    name = ""
+                    icon = "Empty.png"
+                    label = ""
+            elif key == five_base:
+                if connection_allowed:
+                    name = device_5[3]
+                    if load_data:
+                        icon = get_checkbox_pic(get_connect(dev[0], 'b1'))
+                    else:
+                        icon = "checkbox-blank-line.png"
+                    label = "B1"
+                else:
+                    name = ""
+                    icon = "Empty.png"
+                    label = ""
+            elif key == five_down:
+                if connection_allowed:
+                    name = device_5[3]+"-"
+                    if load_data:
+                        icon = get_checkbox_pic(get_connect(dev[0], 'b2'))
+                    else:
+                        icon = "checkbox-blank-line.png"
+                    label = "B2"
+                else:
+                    name = ""
+                    icon = "Empty.png"
+                    label = ""
+            elif key == five_up:
+                if connection_allowed:
+                    name = device_5[3]+"+"
+                    if load_data:
+                        icon = get_checkbox_pic(get_connect(dev[0], 'b3'))
+                    else:
+                        icon = "checkbox-blank-line.png"
+                    label = "B3"
+                else:
+                    name = ""
+                    icon = "Empty.png"
+                    label = ""
     return {
         "name": name,
         "icon": os.path.join(settings.ASSETS_PATH, icon),
         "font": os.path.join(settings.ASSETS_PATH, settings.font),
         "label": label,
         "menu": menu,
-        "device": dev 
+        "device": dev, 
     }
 
 def change_page(deck, page, device=None):
@@ -320,14 +396,24 @@ def key_change_callback(deck, key, state):
             elif key_style["name"] == "one+":
                 device = key_style["device"]
                 run(['pulsemeeter', 'mute', device[0]])
-                print(f'{key_style["device"]} mute toggled')
+                print(f'{key_style["device"][0]} mute toggled')
             elif key_style["name"] == "two+":
                 device = key_style["device"]
+                print(f'toggle eq ({device[0]})')
                 run(['pulsemeeter', 'eq', device[0]])
+            elif key_style["name"] == "one-":
+                device = key_style["device"]
+                print(f'toggle rnnoise {device[0]}')
+                run(['pulsemeeter', 'rnnoise', device[0]])
             elif key_style["name"] == "three+":
                 device = key_style["device"]
-                run(['pulsemeeter', 'rnnoise', device[0]])
-            
+                print(f'made device primary ({device[0]})')
+                run(['pulsemeeter', 'primary', device[0]])
+            elif key_style["name"] in ['four', 'four-', 'four+', 'five', 'five-', 'five+']:
+                device = key_style["device"]
+                out_device = key_style["label"].lower()
+                print(f'toggling connection {device[0]} to {out_device}')
+                run(['pulsemeeter', 'connect', device[0], out_device])
         else:
             check_volume_keys(deck, key_style, "one", device_1)
             check_volume_keys(deck, key_style, "two", device_2)
@@ -351,15 +437,35 @@ def get_devices():
     devices.append(device_5)
     return devices
 
+def update_data_key(key):
+    update_key_image(deck, key, True)
+    print(f'settings data key {key} ({curr_site})')
+
+def update_key(key):
+    update_key_image(deck, key, False)
+    print(f'setting key {key} ({curr_site})')
+
+# I do not really want to make the pool higher because that increases the cpu usage
 def reload_data():
+    use_multiprocessing = settings.use_multiprocessing
     if curr_site == 'main':
-        for key in [one_base, two_base, three_base, four_base, five_base]:
-            update_key_image(deck, key, True)
-            print(f'setting data for key {key} ({curr_site})')
+        updating_keys = [one_base, two_base, three_base, four_base, five_base]
+        if use_multiprocessing:
+            p = mp.Pool(5)
+        else:
+            for key in updating_keys:
+                update_data_key(key)
     elif curr_site == 'sub-menu':
-        for key in [one_up, two_up, three_up]:
-            update_key_image(deck, key, True)
-            print(f'setting data for key {key} ({curr_site})')
+        updating_keys = [one_up, one_down, two_up, three_up, four_base, four_up, four_down, five_base, five_down, five_up]
+        if use_multiprocessing:
+            p = mp.Pool(5)
+        else:
+            for key in updating_keys:
+                update_data_key(key)
+    if use_multiprocessing:
+        p.map(update_data_key, updating_keys)
+        p.close()
+        p.join()
 
 
 
@@ -367,6 +473,15 @@ if __name__ == "__main__":
     streamdecks = DeviceManager().enumerate()
 
     print("Found {} Stream Deck(s).\n".format(len(streamdecks)))
+    if settings.check_running:
+        print('checking if pulsemeeter is running.')
+        try:
+            process = Popen(['pgrep', 'pulsemeeter'], stdout=PIPE)
+        except:
+            print('Could not check if pulsemeeter is runnning')
+        else:
+            if process.communicate()[0].decode().strip() == "":
+                sys.exit("pulsemeeter is not running (if this message is wrong, turn off check_running in settings)")
 
     for index, deck in enumerate(streamdecks):
         deck.open()
@@ -375,7 +490,7 @@ if __name__ == "__main__":
         print("Opened '{}' device (serial number: '{}')".format(deck.deck_type(), deck.get_serial_number()))
 
         # Set initial screen brightness to 30%.
-        deck.set_brightness(30)
+        deck.set_brightness(settings.brightness)
 
         print('setting key variables')
         # I could probably replace this with a loop but I don't really find it necessary
@@ -418,9 +533,17 @@ if __name__ == "__main__":
 
         # Set initial key images.
         a = range(deck.key_count())
-        for key in a:
-            print(f'creating key image {key}')
-            update_key_image(deck, key, False)
+        if settings.use_multiprocessing:
+            print('multiprocessing is turned on')
+            p = mp.Pool(4)
+            p.map(update_key, a)
+            p.close()
+            p.join()
+        else:
+            print('multiprocessing is turned off.')
+            for key in a:
+                print(f'creating key image {key}')
+                update_key_image(deck, key, False)
         reload_data()
         # Register callback function for when a key state changes.
         deck.set_key_callback(key_change_callback)
--- settings.py
+++ settings.py
@@ -2,25 +2,43 @@ import os
 
 # Folder location of image assets used 
 ASSETS_PATH = os.path.join(os.path.dirname(__file__), "Assets")
+# higher cpu usage but faster data loading
+use_multiprocessing = True
+check_running = True
 
-# only use 4 characters for the label at the standard size or shrink the font size
+# ----------------------------------------------------------------
+# CONFIGURATION
 
 # how much to increase or decrease the gain
 volume_step = 10
+# the brightness of the streamdeck
+brightness = 30
+
 # show the volume step on the keys
 show_volume_step = False
+# show the device name on the device page
+show_device = False
 
-# [device], [type (mic|speaker)], [label], [internal info (do not change)]
+# [device], [(mic|speaker)], [label], [internal info (do not change)]
 # just set the device to None to disable it
 # example:
 # device_1 = None
+# it also supports \n for labels
 
-device_1 = ['hi1', 'mic', 'Hi1', 'one']
-device_2 = ['b1', 'speaker', 'B1', 'two']
-device_3 = ['vi1', 'speaker', 'Vi1', 'three']
-device_4 = ['a1', 'speaker', 'A1', 'four']
-device_5 = ['a2', 'speaker', 'A2', 'five']
+device_1 = ['hi1', 'mic', 'LABEL', 'one']
+device_2 = ['vi2', 'speaker', 'LABEL2', 'two']
+device_3 = ['vi1', 'speaker', 'LABEL3', 'three']
+device_4 = ['a1', 'speaker', 'LABEL4', 'four']
+device_5 = ['b1', 'mic', 'LABEL5', 'five']
+
+# the seperator used between the volume and your label (can also be \n)
+label_seperator = "-"
+# change the margins of the icon (you probably only want to change the first value for the size)
+margins = [25, 0, 10, 0]
 
 # remember to put the font in the assets folder
+# font used in examples:
+# font = "JetBrains_Mono_Medium_Nerd_Font_Complete.ttf"
 font = "Roboto-Regular.ttf"
-font_size = 14
+
+font_size = 12
